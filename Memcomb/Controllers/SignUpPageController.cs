b925a8e6cbb12e881892cf8553c189b2ba5286d5
Noah Bradley <noahbradley@oakland.edu>
2020-01-21 09:30:21 -0500

    Sign Up Page
    I added the sign up page, needs finalzing but heres the foundation of it

---
+++ Memcomb/Controllers/SignUpPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Memcomb.Controllers
+{
+    public class SignUpPageController : Controller
+    {
+        // GET: SignUpPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
