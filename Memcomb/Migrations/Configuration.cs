de90ce50054d3296db4710e044c2f9ecac9995d9
Noah Bradley <noahbradley@oakland.edu>
2020-01-21 18:54:46 -0500

    The Database Works!!

---
+++ Memcomb/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace Memcomb.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Memcomb.Models.SignUpDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Memcomb.Models.SignUpDBContext";
+        }
+
+        protected override void Seed(Memcomb.Models.SignUpDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
