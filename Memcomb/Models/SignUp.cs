f8106e4239a62594df4b599cdf648e7b0b16d07e
Noah Bradley <noahbradley@oakland.edu>
2020-01-21 14:15:16 -0500

    Added a model for DB

---
+++ Memcomb/Models/SignUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Memcomb.Models
+{
+	public class SignUp
+	{
+		public int ID { get; set; }
+		public string Title { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string Genre { get; set; }
+		public decimal Price { get; set; }
+	}
+
+	public class SignUpDBContext : DbContext
+	{
+		public DbSet<SignUp> Signees { get; set; }
+	}
+}
