3e777a017c2a4c41a3c220e5d2eeb11a892005a5
JiZhang321 <Jzhang3@oakland.edu>
2020-01-29 15:32:37 -0500

    Profile page updated again

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace Memcomb.Controllers
 {
@@ -13,7 +14,30 @@ namespace Memcomb.Controllers
         {
             return View();
         }
-
+        [HttpGet]
+        public ActionResult UploadFile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UploadFile(HttpPostedFileBase file)
+        {
+            try
+            {
+                if(file.ContentLength > 0)
+                {
+                    string _FileName = Path.GetFileName(file.FileName);
+                    string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                    file.SaveAs(_path);
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message("Invalid file!");
+                return View();
+            }
+        }
         // GET: Profile/Details/5
         public ActionResult Details(int id)
         {
